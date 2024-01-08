434c81989975101543c52f264835c5e30ad5cc5d
Deep Sarker <deep.sarker@nexgencloud.com>
2024-01-08 23:03:19 +0600

    test release

--- components/Main/Home.js
+++ components/Main/Home.js
@@ -7,7 +7,7 @@ const HomeComponent = ()=>{
             <div>
                 <div className={"bg-hero-content bg-contain h-136 flex flex-col items-center justify-around"}>
                     <div className={"flex flex-col items-center justify-center gap-8"}>
-                        <div className={"text-white text-6xl"}><strong>Deep</strong> Vision</div>
+                        <div className={"text-white text-6xl"}><strong>Tonmoy</strong> Vision</div>
                         <div className={"text-white font-light text-lg"}>A natural language processing platform for
                             building state-of-the-art models
                         </div>
